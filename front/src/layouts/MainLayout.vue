f77b1e85e22ab259e97d79d805443b193f18ca42
ecdns <clementpavot78@gmail.com>
2023-11-18 09:42:36 +0300

    set up front

---
+++ front/src/layouts/MainLayout.vue
@@ -0,0 +1,97 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header class="header" elevated>
+      <q-toolbar class="toolbar grid col-12">
+        <div class="col-4"></div>
+        <div class="col-4">
+          <q-list class="links-list flex items-center justify-center">
+            <q-btn class="header-links q-hoverable" v-for="(link, index) in linksList" v-bind:key="index" flat>{{
+              link.title }}</q-btn>
+          </q-list>
+        </div>
+        <div class="flex justify-center col-4">
+          <q-btn class="wallet-button" outlined rounded><svg class="wallet-icon" xmlns="http://www.w3.org/2000/svg"
+              height="1em"
+              viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+              <path
+                d="M64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V192c0-35.3-28.7-64-64-64H80c-8.8 0-16-7.2-16-16s7.2-16 16-16H448c17.7 0 32-14.3 32-32s-14.3-32-32-32H64zM416 272a32 32 0 1 1 0 64 32 32 0 1 1 0-64z" />
+            </svg>Connect Wallet</q-btn>
+        </div>
+      </q-toolbar>
+    </q-header>
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+
+export default {
+  setup() {
+    const linksList = [
+      {
+        title: 'Home',
+        caption: 'quasar.dev',
+        icon: 'school',
+        link: 'https://quasar.dev'
+      },
+      {
+        title: 'Catalogue',
+        caption: 'github.com/quasarframework',
+        icon: 'code',
+        link: 'https://github.com/quasarframework'
+      },
+      {
+        title: 'Security',
+        caption: 'chat.quasar.dev',
+        icon: 'chat',
+        link: 'https://chat.quasar.dev'
+      },
+      {
+        title: 'Contact',
+        caption: 'forum.quasar.dev',
+        icon: 'record_voice_over',
+        link: 'https://forum.quasar.dev'
+      }]
+    return { linksList }
+  }
+}
+
+
+
+</script>
+
+
+<style lang="scss">
+.header {
+  background-color: white;
+  width: 100%;
+}
+
+.toolbar {
+  width: 100%;
+}
+
+.header-links {
+  color: black;
+}
+
+.links-list {
+  height: 120px;
+}
+
+.links-list:hover {
+  background: none;
+}
+
+.wallet-button {
+  color: white;
+  background-color: #4386dd;
+}
+
+.wallet-icon {
+  fill: #ffffff;
+  margin-right: 3px;
+}
+</style>
